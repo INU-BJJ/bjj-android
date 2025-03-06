49809a5fa6861e67deef9de837f2ca4cd90ee0bc
sk5514224@gmail.com <sk5514224@gmail.com>
2025-03-06 14:33:10 +0900

    [feat] 기본 뷰모델 설정 및 좋아요한 메뉴 데이터 관련 함수 작성

--- app/src/main/java/inu/appcenter/bjj_android/ui/mypage/component/SwitchButton.kt
+++ app/src/main/java/inu/appcenter/bjj_android/ui/mypage/component/SwitchButton.kt
@@ -6,22 +6,19 @@ import androidx.compose.material3.Switch
 import androidx.compose.material3.SwitchDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.graphics.Color
 import inu.appcenter.bjj_android.ui.theme.Orange_FF7800
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun SwitchButton() {
-    var checked by remember { mutableStateOf(true) }
-
+fun SwitchButton(
+    checked: Boolean = true,
+    onCheckedChange: (Boolean) -> Unit = {}
+) {
     CompositionLocalProvider(LocalMinimumInteractiveComponentEnforcement provides false) {
         Switch(
             checked = checked,
-            onCheckedChange = { checked = it },
+            onCheckedChange = onCheckedChange,
             colors = SwitchDefaults.colors(
                 checkedThumbColor = Color.White,
                 checkedTrackColor = Orange_FF7800
